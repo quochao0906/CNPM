7099c102ba24a4cf6c7a3d0b99abb680b97d4311
Duong Quoc Hao <duongquochao09061996@gmail.com>
2018-01-10 13:34:35 +0700

    php
    đã cập nhật thêm php

---
+++ contact.php
@@ -0,0 +1,11 @@
+<?php
+	include "header.php";
+?>
+	<div class="main">
+			<p><h2>Contact Us:</h2></p>
+			<p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1385.6170911124204!2d106.71438471814398!3d10.80235281819632!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317528a27c890ee3%3A0xd7f92a7c003cbff5!2zSFVURUNIIC0gVHJ1bmcgdMOibSBWxINuIGjDs2EgTmdo4buHIHRodeG6rXQ!5e0!3m2!1svi!2s!4v1514770794183" frameborder="0" allowfullscreen></iframe></p>
+		
+			<p><h3>Công ty 3H</h3>
+<?php
+	include "footer.php";
+?>
---
+++ login.php
@@ -0,0 +1,30 @@
+<?php
+	include "header.php";
+?>
+<div class="login" >
+ 			<table >
+ 				<tr >
+ 					<th colspan="2">ĐĂNG NHẬP</th>
+ 				</tr>
+ 				<tr >
+ 					<td>Tài khoản:</td>
+ 					<td><input type="text" name="username" placeholder="Tên Đăng Nhập" ></td>
+ 				</tr>
+
+ 				<tr >
+ 					<td>Mật khẩu:</td>
+ 					<td><input type="password" name="password" placeholder="Mật Khẩu" ></td>
+ 				</tr>
+ 				<tr >
+ 					<td><a href="/">Quên mật khẩu?</a></td>
+ 					<td><button type="button" >Đăng Nhập </button</td>
+				</tr>
+				<tr >
+					<td><a href="register.html">Bạn chưa đăng kí tài khoản?</a></td>
+				</tr>
+ 			</table>
+ 			
+ 	</div>
+<?php
+	include "footer.php";
+?>
